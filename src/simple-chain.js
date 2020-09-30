961b7e125d876e56dfaef4df63856446ddaba3fc
Anton <toshi4ka7@mail.ru>
2020-09-30 22:52:07 +0300

    feat: ready task simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,59 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  flag: false,
+  str: '',
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.clean();
+    if (this.str === '') return 0;
+    let count = this.str.split('~~').length;
+    return count;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.clean();
+    if (arguments.length === 0) {
+      value = '';
+    }
+    if (this.str === '') {
+      this.str += `( ${value} )`;
+    } else {
+      this.str += `~~( ${value} )`;
+    }
+   return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.clean();
+    if (arguments.length === 0 || 
+      typeof(position) !== 'number' || 
+      Number.isNaN(position) || 
+      !Number.isInteger(position) ||
+      position <= 0 ||
+      position > this.getLength()) {
+        this.flag = true;
+        throw new Error('error in position');
+      }
+
+      let array = this.str.split('~~');
+      array.splice(position - 1, 1);
+      this.str = array.join("~~");
+
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.clean();
+    this.str = this.str.split('~~').reverse().join("~~");
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    this.flag = true;
+    return this.str;
+  },
+  clean() {
+    if (this.flag) {
+      this.str = '';
+      this.flag = false;
+    }
+  },
 };
 
 module.exports = chainMaker;

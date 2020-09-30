e209bdc2a1b137cef0466d2f3a42b82105dbfec8
Anton Dovgun <toshi4ka7@mail.ru>
2020-09-30 11:13:03 +0300

    feat: ready task dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let name = '';
+
+  for (i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === 'string') {
+      name +=  members[i].trim()[0];
+    }
+  }
+
+  name = name.toUpperCase().split("").sort().join("");
+
+  return name;
 };

d99d9471f687afb772c56f0ae31132f49b9c295a
Anton Dovgun <toshi4ka7@mail.ru>
2020-09-30 10:18:14 +0300

    feat: ready task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,15 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) !== 'string') {
+    return false;
+  }
+  if (typeof(+sampleActivity) !== NaN) {
+    return false;
+  }
+
+  const k = 0.693 / HALF_LIFE_PERIOD;
+
+  return Math.ceil(((Math.log(MODERN_ACTIVITY / +sampleActivity)) / k));
 };

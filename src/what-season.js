64539905ee5eb799ec36948da987a854c7ca2f36
Anton Dovgun <toshi4ka7@mail.ru>
2020-09-30 11:59:29 +0300

    feat: ready task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (!(date instanceof Date)) {
+    throw new Error('argument is not Data');
+  }
+
+  date.getYear();
+
+  const month = date.getMonth();
+
+  switch (month) {
+    case 11:
+      return 'winter';
+      break;
+    case 0:
+      return 'winter';
+      break;
+    case 1:
+      return 'winter';
+      break;
+    case 2:
+      return 'spring';
+      break;
+    case 3:
+      return 'spring';
+      break;
+    case 4:
+      return 'spring';
+      break;
+    case 5:
+      return 'summer';
+      break;
+    case 6:
+      return 'summer';
+      break;
+    case 7:
+      return 'summer';
+      break;
+    case 8:
+      return 'autumn';
+      break;
+    case 9:
+      return 'autumn';
+      break;
+    case 10:
+      return 'autumn';
+      break;
+  }
 };

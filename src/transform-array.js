1e5b8e1b88afbae09882da7b609cca94d961be85
Anton Dovgun <toshi4ka7@mail.ru>
2020-09-30 16:16:18 +0300

    feat: ready task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error('argument is not Array');
+  }
+
+  let newArray = [];
+
+  for (i = 0; i < arr.length; i++) {
+
+    if (i === 0) {
+      if (arr[i] === '--discard-prev' || arr[i] === '--double-prev') {
+        continue;
+      }
+    }
+
+    if (i === arr.length - 1) {
+      if (arr[i] === '--double-next' || arr[i] === '--discard-next') {
+        continue;
+      }
+    }
+
+    if (arr[i] === '--discard-next') {
+      i++;
+    } else if (arr[i] === '--discard-prev') {
+      if (!(arr[i - 2] === '--discard-next')) {
+        newArray.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      newArray.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev') {
+      if (!(arr[i - 2] === '--discard-next')) {
+        newArray.push(arr[i - 1]);
+      }
+      
+    } else {
+      newArray.push(arr[i]);
+    }
+
+
+  }
+
+  return newArray;
 };

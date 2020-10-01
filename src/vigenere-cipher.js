00a84c0a1515e50a808f483c0e1f3030ae90b10c
Anton <toshi4ka7@mail.ru>
2020-10-02 00:15:47 +0300

    feat: ready task vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,91 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  alf = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  mode = true;
+
+  constructor(flag = true) {
+    this.mode = flag;
+  }
+
+
+  encrypt(message, key) {
+    if (arguments.length < 2) {
+      throw new Error('error in argument');
+    }
+
+    let strArray = message.toUpperCase().split('');
+    let strKey = key.toUpperCase().split('');
+
+    let strArrayKey = [];
+    let j = 0;
+    for (let i = 0; i < strArray.length; i++) {
+      if (this.alf.includes(strArray[i])) {
+        strArrayKey[i] = [strArray[i], strKey[j]];
+        j++;
+        if (j === strKey.length) {
+          j = 0;
+        }
+      } else {
+        strArrayKey[i] = strArray[i];
+      }
+    }
+
+    let strArrayDefend = [];
+    for (let i = 0; i < strArrayKey.length; i++) {
+      if (strArrayKey[i] instanceof Array) {
+        let pos = (this.alf.indexOf(strArrayKey[i][0]) + this.alf.indexOf(strArrayKey[i][1])) % 26;
+        strArrayDefend[i] = this.alf[pos];
+      } else {
+        strArrayDefend[i] = strArrayKey[i];
+      }
+    }
+
+    if (!this.mode) {
+      strArrayDefend.reverse();
+    }
+
+    return strArrayDefend.join('');
+  } 
+
+  decrypt(message, key) {
+    if (arguments.length < 2) {
+      throw new Error('error in argument');
+    }
+
+    let strArray = message.toUpperCase().split('');
+    let strKey = key.toUpperCase().split('');
+
+    let strArrayKey = [];
+    let j = 0;
+    for (let i = 0; i < strArray.length; i++) {
+      if (this.alf.includes(strArray[i])) {
+        strArrayKey[i] = [strArray[i], strKey[j]];
+        j++;
+        if (j === strKey.length) {
+          j = 0;
+        }
+      } else {
+        strArrayKey[i] = strArray[i];
+      }
+    }
+
+    let strArrayDefend = [];
+    for (let i = 0; i < strArrayKey.length; i++) {
+      if (strArrayKey[i] instanceof Array) {
+        let pos = (this.alf.indexOf(strArrayKey[i][0]) - this.alf.indexOf(strArrayKey[i][1]) + 26) % 26;
+        strArrayDefend[i] = this.alf[pos];
+      } else {
+        strArrayDefend[i] = strArrayKey[i];
+      }
+    }
+
+    if (!this.mode) {
+      strArrayDefend.reverse();
+    }
+
+    return strArrayDefend.join('');
   }
 }
 

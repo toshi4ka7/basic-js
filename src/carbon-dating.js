5ab45608d1e18e9541816cc20b5b0f573262f31e
Anton Dovgun <toshi4ka7@mail.ru>
2021-03-19 14:45:09 +0300

    feat: tasks are ready

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,15 +3,14 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity) {
-  if (typeof(sampleActivity) !== 'string') {
-    return false;
-  }
-  if (typeof(+sampleActivity) !== NaN) {
-    return false;
-  }
+module.exports = function dateSample(value) {
+  if (typeof value !== 'string') return false
+  if (Number.isNaN(+value)) return false
+  if ((+value) + '' !== value) return false
+  if (value == 0) return false
 
   const k = 0.693 / HALF_LIFE_PERIOD;
-
-  return Math.ceil(((Math.log(MODERN_ACTIVITY / +sampleActivity)) / k));
+  const res = Math.ceil(((Math.log(MODERN_ACTIVITY / +value)) / k))
+  if (Number.isNaN(res)) return false
+  return res <= 0 ? false : res
 };
